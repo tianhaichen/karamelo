9be19f540f978795b30f1024df2051d00f804185
cz <cth269@outlook.com>
2024-03-20 11:04:17 +0800

    update zlib source files

---
+++ third-party/zlib-1.3.1/contrib/vstudio/vc17/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/cth26/Downloads/zlib-1.3.1
+# Build directory: C:/Users/cth26/Downloads/zlib-1.3.1/contrib/vstudio/vc17
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/cth26/Downloads/zlib-1.3.1/CMakeLists.txt;203;add_test;C:/Users/cth26/Downloads/zlib-1.3.1/CMakeLists.txt;0;")
